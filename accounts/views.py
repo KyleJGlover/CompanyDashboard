8d2835503ac1565aedb9f28824457112fb1036b3
Kyle J Glover <kjglover4585@gmail.com>
2023-10-05 18:25:13 -0700

    FIrst Commit

---
+++ accounts/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse(request, 'accounts/dashboard.html')
+
+def products(request):
+    return HttpResponse('accounts/products.html')
+
+def customer(request):
+    return HttpResponse('accounts/customer.html')
