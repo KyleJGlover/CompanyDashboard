c22ea15d494c614de74b4d9c1ed028061b4a7134
Kyle J Glover <kjglover4585@gmail.com>
2023-10-31 15:41:27 -0700

    Adding boiler plate layout for the dashboard, products, and customer info pages. updated url patterns inline with django. Cleanded up UI look and feel.

--- Dashboard/settings.py
+++ Dashboard/settings.py
@@ -15,7 +15,8 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+# Directory of navbar template at the base of the project folder
+#TEMP_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
