65b99ece37c0be8e503fbc839ac44ce5fc950a4c
Kyle J Glover <kjglover4585@gmail.com>
2023-11-06 07:22:38 -0800

    Update settings.py
    Removed info

--- Dashboard/settings.py
+++ Dashboard/settings.py
@@ -144,4 +144,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'kjglover4585@gmail.com'
-EMAIL_HOST_PASSWORD = 'gaxa yond wdgz efva'
+EMAIL_HOST_PASSWORD = ''
